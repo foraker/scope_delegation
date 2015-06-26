f22feb516a19412c080efb958e1e3ce96c375e1c
Jon Evans <jle@foraker.com>
2015-06-26 13:00:38 -0600

    Add a basic spec
    At least make sure the syntax is valid.

--- spec/scope_delegation_spec.rb
+++ spec/scope_delegation_spec.rb
@@ -1,7 +1,24 @@
 require 'spec_helper'
 
 describe ScopeDelegation do
+  let(:association) { double }
+
+  class ActiveRecordDouble
+    extend ScopeDelegation
+
+    def self.reflect_on_association(association)
+      association
+    end
+  end
+
   it 'has a version number' do
     expect(ScopeDelegation::VERSION).not_to be nil
+  end
+
+  describe '.delegate_scope' do
+    it 'calls scope' do
+      expect(ActiveRecordDouble).to receive(:scope)
+      ActiveRecordDouble.delegate_scope :some_scope, to: :association
+    end
   end
 end
