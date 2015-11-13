438601a367b5eada805c732d84a217366be83177
ballauriena <llc@foraker.com>
2015-11-13 16:00:46 -0700

    Add ability to group delegated scope

--- lib/scope_delegation.rb
+++ lib/scope_delegation.rb
@@ -5,9 +5,14 @@ module ScopeDelegation
     source      = options[:scope] || name
     association = reflect_on_association(options[:to]) || raise("Unknown association")
     name        = [options[:prefix], name].compact.join("_")
+    grouping    = options[:group]
 
     scope name, Proc.new { |*args|
-      joins(association.name).merge(association.klass.send(source, *args))
+      if grouping
+        joins(association.name).merge(association.klass.send(source, *args)).group(grouping)
+      else
+        joins(association.name).merge(association.klass.send(source, *args))
+      end
     }
   end
 end
